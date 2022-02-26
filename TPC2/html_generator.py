0b36052470097e40d90be8fbab37bfb820949be7
tiago10cf@hotmail.com <tiago10cf@hotmail.com>
2022-02-26 00:12:01 +0000

    TPC2 feito

--- TPC2/html_generator.py
+++ TPC2/html_generator.py
@@ -1,4 +1,5 @@
 import json
+from operator import index
 
 movies = {}
 actors = {}
@@ -11,12 +12,16 @@ def home_page_generator():
 <html lang="pt">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <title>TPC2 - Indice</title>
     </head>
     <body>
-        <h1><a href="http://localhost:7777/filmes">Filmes</a></h1>
-        <h1><a href="http://localhost:7777/atores">Atores</a></h1>
+        <ul class="w3-ul w3-border">
+            <li class="w3-black"><h1>TPC2 - Indice</h1></li>
+            <li class="w3-hover-grey"><h2><a href="http://localhost:7777/filmes">Filmes</a></h2></li>
+            <li class="w3-hover-grey"><h2><a href="http://localhost:7777/atores">Atores</a></h2></li>
+        </ul>
     </body>
 </html>"""
     page.write(index_html)
@@ -27,20 +32,24 @@ def movies_page_generator():
 <html lang="pt">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <title>TPC2 - Filmes</title>
     </head>
     <body>
-        <h1>Filmes</h1>
-        <ul>"""
+        <div class="w3-bar w3-black">
+            <a href="http://localhost:7777/" class="w3-bar-item w3-button">Indice</a>
+            <a href="http://localhost:7777/filmes" class="active w3-bar-item w3-button">Filmes</a>
+            <a href="http://localhost:7777/atores" class="w3-bar-item w3-button">Atores</a>
+        </div>
+        <ul class="w3-ul">
+            <li class="w3-dark-grey"><h2>Filmes</h2></li>"""
 
-    n = 1
     for m in movies:
-        nr = str(n)
-        movie_html = "\n\t\t\t<li><a href=\"http://localhost:7777/filmes/f" + nr + "\">" + m + " - " + str(movies[m][0]) + "</a></li>"
+        nr = str(movies[m][0])
+        movie_html = "\n\t\t\t<li class=\"w3-hover-grey\"><a href=\"http://localhost:7777/filmes/f" + nr + "\">-> " + m + " - " + str(movies[m][1]) + "</a></li>"
         movies_html += movie_html
         movie_page_generator(nr,m)
-        n+=1
 
     movies_html += """
         </ul>
@@ -54,20 +63,24 @@ def actors_page_generator():
 <html lang="pt">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <title>TPC2 - Atores</title>
     </head>
     <body>
-        <h1>Atores</h1>
-        <ul>"""
+        <div class="w3-bar w3-black">
+            <a href="http://localhost:7777/" class="w3-bar-item w3-button">Indice</a>
+            <a href="http://localhost:7777/filmes" class="w3-bar-item w3-button">Filmes</a>
+            <a href="http://localhost:7777/atores" class="active w3-bar-item w3-button">Atores</a>
+        </div>
+        <ul class="w3-ul">
+            <li class="w3-dark-grey"><h2>Atores</h2></li>"""
 
-    n = 1
     for a in actors:
-        nr = str(n)
-        actor_html = "\n\t\t\t<li><a href=\"http://localhost:7777/atores/a" + nr + "\">" + a + "</a></li>"
+        nr = str(actors[a][0])
+        actor_html = "\n\t\t\t<li class=\"w3-hover-grey\"><a href=\"http://localhost:7777/atores/a" + nr + "\">" + a + "</a></li>"
         actors_html += actor_html
         actor_page_generator(nr,a)
-        n += 1
 
     actors_html += """
         </ul>
@@ -82,26 +95,37 @@ def movie_page_generator(nr,m):
 <html lang="pt">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <title>TPC2 - """ + m + """</title>
     </head>
     <body>
-        <h1>""" + m + """</h1>
-        <h3>Ano: """ + str(movies[m][0]) + """</h3>
-        <h3>Elenco:</h3>
-        <ul>"""
-    for a in movies[m][1]:
-        movie_html += "\n\t\t\t<li>" + a + "</li>"
+        <div class="w3-bar w3-black">
+            <a href="http://localhost:7777/" class="w3-bar-item w3-button">Indice</a>
+            <a href="http://localhost:7777/filmes" class="w3-bar-item w3-button">Filmes</a>
+            <a href="http://localhost:7777/atores" class="w3-bar-item w3-button">Atores</a>
+        </div>
+        <ul class="w3-ul">
+            <li class="w3-dark-grey"><h2>""" + m + """</h2></li>
+        </ul>
+        <div class="w3-container">
+            <h4><b>Ano:</b> """ + str(movies[m][1]) + """</h4>
+            <h4><b>Elenco:</b></h4>
+            <ul class="w3-ul">"""
+    for a in movies[m][2]:
+        nr = str(actors[a][0])
+        movie_html += "\n\t\t\t\t<li class=\"w3-hover-grey\">" + "<a href=\"http://localhost:7777/atores/a" + nr + "\">" + a + "</a></li>"
     
     movie_html += """
-        </ul>
-        <h3>Género(s)</h3>
-        <ul>"""
-    for g in movies[m][2]:
-        movie_html += "\n\t\t\t<li>" + g + "</li>"
+            </ul>
+            <h4><b>Género(s):</b></h4>
+            <ul class="w3-ul">"""
+    for g in movies[m][3]:
+        movie_html += "\n\t\t\t\t<li>" + g + "</li>"
 
     movie_html += """
-        </ul>
+            </ul>
+        </div>
     </body>
 </html>"""
     page.write(movie_html)
@@ -113,18 +137,29 @@ def actor_page_generator(nr,a):
 <html lang="pt">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <title>TPC2 - """ + a + """</title>
     </head>
     <body>
-        <h1>""" + a + """</h1>
-        <h3>Filmes:</h3>
-        <ul>"""
-    for m in actors[a]:
-        actor_html += "\n\t\t\t<li>" + m + "</li>"
+        <div class="w3-bar w3-black">
+            <a href="http://localhost:7777/" class="w3-bar-item w3-button">Indice</a>
+            <a href="http://localhost:7777/filmes" class="w3-bar-item w3-button">Filmes</a>
+            <a href="http://localhost:7777/atores" class="w3-bar-item w3-button">Atores</a>
+        </div>
+        <ul class="w3-ul">
+            <li class="w3-dark-grey"><h2>""" + a + """</h2></li>
+        </ul>
+        <div class="w3-container">
+            <h4><b>Filmes:</b></h4>
+            <ul class="w3-ul">"""
+    for m in actors[a][1]:
+        nr = str(movies[m][0])
+        actor_html += "\n\t\t\t\t<li class=\"w3-hover-grey\">" + "<a href=\"http://localhost:7777/filmes/f" + nr + "\">" + m + "</a></li>"
     
     actor_html += """
-        </ul>
+            </ul>
+        </div>
     </body>
 </html>"""
     page.write(actor_html)
@@ -132,25 +167,33 @@ def actor_page_generator(nr,a):
 def valid_name(name):
     return ((name[0].isupper() or name[0] == 5)) and len(name) > 2 and name[1] != " "
 
-def add_actor(a,m):
-    if valid_name(a):
-        if a in actors:
-            actors[a].append(m)
-        else:
-            actors[a] = [m]
+def cast_filter(cast):
+    aux = []
+    for c in cast:
+        if valid_name(c): aux.append(c)
+    return sorted(aux)
 
-def add_movie(m):
-    movies[m['title']] = [m['year'],m['cast'],m['genres']]
+i = 1
+j = 1
 
 for m in data:
-    add_movie(m)
+    movies[m['title']] = [i,m['year'],cast_filter(m['cast']),sorted(m['genres'])]
+    i += 1  
     for a in m['cast']:
-        add_actor(a,m['title'])
+        if valid_name(a):
+            if a in actors:
+                actors[a][1].append(m['title'])
+            else:
+                actors[a] = [j,[m['title']]]
+                j += 1
+    for a in actors:
+        actors[a][1] = sorted(actors[a][1])
 
 movies = dict(sorted(movies.items(), key = lambda x : x[0].lower()))
 actors = dict(sorted(actors.items(), key = lambda x : x[0].lower()))
 
-
+#print(movies)
+#print(actors)
 home_page_generator()
 movies_page_generator()
 actors_page_generator()
